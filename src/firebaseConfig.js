100f50a75cd1b526d105b6106dd5db5ff3bb0080
Moonwalker <abhisk.mhra@gmail.com>
2021-04-13 11:27:50 +0530

    FirebaseConfig file setup

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBtCk1x1DuXCcHibtg18i5Il8tZ3UsuIKY",
+    authDomain: "todoreactapp-c68b6.firebaseapp.com",
+    projectId: "todoreactapp-c68b6",
+    storageBucket: "todoreactapp-c68b6.appspot.com",
+    messagingSenderId: "375073367504",
+    appId: "1:375073367504:web:d1313558d9046374e7c5d5"
+  };
