fcef3f4bc2be04a4dae9a5a141901c3ad73781bb
Moonwalker <abhisk.mhra@gmail.com>
2021-04-13 12:36:23 +0530

    added material UI button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import {useState} from 'react';
+import { Button } from '@material-ui/core';
+
+
 function App() {
- 
-const [state,setState] = useState(initialState);
+  const [todoInput, setTodoInput] = useState("");
+  
+  function addTodo(value){
+    value.preventDefault();
+  console.log("tdo");
+  }
   return (
     <div className="App">
     <h1>TODO's App 📝</h1>
-    <TextField id="standard-basic" label="What's your next goal ?" />
-    <h2>Things you need to do today 👇🏻</h2>
+   <form>
+   <TextField id="standard-basic" 
+    value={todoInput} 
+    onChange={(input)=>setTodoInput(input.target.value)}
+    label="What's your next goal ?" />
+    <Button
+     type="submit"
+     variant="contained"
+     onClick={addTodo}>Add</Button>
+   </form> <h2>Things you need to do today 👇🏻</h2>
     </div>
   );
 }
