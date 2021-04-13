be52a9ec7597a901ca96c22d10dfd2903f423521
Moonwalker <abhisk.mhra@gmail.com>
2021-04-13 14:05:43 +0530

    Added functions for get and add todos to firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 
 import './App.css';
 import TextField from '@material-ui/core/TextField';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {db} from './firebaseConfig';
 import { Button } from '@material-ui/core';
 import firebase from "firebase";
 
 function App() {
   const [todoInput, setTodoInput] = useState("");
-  
-  function addTodo(value){
-    value.preventDefault();
-    
+  function addTodo(e){
+    e.preventDefault();
     // app -> firestore
     db.collection("Todos").add({
       is_in_progress: true,
       // get server timestamp from firestore itself
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      todo: value,
+      todo: todoInput,
+    })
+    // finally setting the todo input to blank for new todo entries from user
+    setTodoInput("");
+  }
+
+  
+
+  // we should get todos to be displayed at the start of the application on screen
+    useEffect(() => {
+      effect
+      return () => {
+        cleanup
+      }
+    }, [])
+    // leaving [] blank as we wanna run this for first time only at time of display
+  
+  
+    const [listTodos, setlistTodos] = useState([]);
+    function getTodos(){
+    // using onSnapshot for real time updates 
+    db.collection("Todos").onSnapshot(function (querySnapshot){
+      // mapping querySnapshots one by one into listTodos using map()
+   setlistTodos(   querySnapshot.docs.map((doc)=>({
+    id: doc.id,
+    todo: doc.data().todo,
+    inprogress: doc.data().is_in_progress,
+    timestamp: doc.data().timestamp
+  })));
     })
   }
+
   return (
     <div className="App">
     <h1>TODO's App 📝</h1>
@@ -40,3 +67,4 @@ function App() {
 }
 
 export default App;
+ 
